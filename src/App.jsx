72214c898b7132446437dfe6d431d2893a94436a
Sabiq Hashil <99120128+SabiqHashil@users.noreply.github.com>
2024-11-11 00:50:48 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Head from "./component/Head";
 function App() {
   return (
     // <div className="  md:bg-gradient-to-r md:from-[#a555ec] md:from-30% md:via-[#d09cfa] md:via-80% md:to-[#f3ccff] bg-gradient-to-b from-[#a555ec] from-30% via-[#d09cfa] via-80% to-[#c87ddf] flex flex-col items-center justify-between h-dvh w-[100%]">
-    <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+    <div className="absolute inset-0 -z-10 h-fit w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
       <div className="flex flex-col items-center justify-center md:gap-28 gap-10 flex-grow  ">
         <Head />
         <Countdown />
